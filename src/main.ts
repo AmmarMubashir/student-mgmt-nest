e5904e0ace9a9996bf5247757a4aaf4b630831df
AmmarMubashir <muhammadammarmubashir@gmail.com>
2025-12-22 17:49:08 +0500

    CORS modified

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "https://student-mgmt-mauve.vercel.app/",
-      // "http://localhost:3000",
-    ],
+    origin: ["https://student-mgmt-mauve.vercel.app/", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // IMPORTANT if using cookies/JWT
